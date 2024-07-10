6aa09f4cf7c3f521436688be49d27a40dd07b948
Cecilia Bergamini <ceciliabergamini@MBP-di-Cecilia.homenet.telecomitalia.it>
2024-07-10 22:12:20 +0200

    aggiornamento ceci

--- Project/src/Utils.cpp
+++ Project/src/Utils.cpp
@@ -54,14 +54,14 @@ array <double,3> barycenter(const MatrixXd& m, unsigned int num_vertices)
 //funzione sfere
 bool check_sphere(const array<double,3> bar1, const array<double,3> bar2, const double l1, const double l2)
 {
-    //controlliamo la distanza tra i due baricentri
+    //controllo la distanza tra i due baricentri
     double distance_bar = 0.0;
     distance_bar = sqrt( (bar1[0] - bar2[0])*(bar1[0] - bar2[0]) +
                          (bar1[1] - bar2[1])*(bar1[1] - bar2[1]) +
                          (bar1[2] - bar2[2])*(bar1[2] - bar2[2]) );
 
     double max_distance = 0.0;
-    max_distance = (l1 + l2) ;///h
+    max_distance = (l1 + l2) ;
 
     if (distance_bar > max_distance){
         return false; // le fratture non si intersecano
@@ -93,37 +93,36 @@ Vector3d normal_vector(const MatrixXd& m)
 
 }
 
-// function to implement solution of systems 3x3 to find planes intersections
+// funzione implementata per risolvere un sistema 3x3 per trovare l’intersezione fra piani
 bool intersezione_piani (Vector3d& n1,
-                     Vector3d& n2,
-                     array <double, 3>& b1,
-                     array <double, 3>& b2,
-                     Vector3d& t,
-                     Vector3d& Point){
+                        Vector3d& n2,
+                        array <double, 3>& b1,
+                        array <double, 3>& b2,
+                        Vector3d& t,
+                        Vector3d& Point){
     double d1 = 0.0; //termine noto piano 1
     double d2 = 0.0; //termine noto piano 2
-    //termine noto piano d'intersezione = 0 come prodotto scalare tra vettore normale
-    // al piano e punto generico della frattura (abbiamo scelto il baricentro)
+    // termine noto piano d'intersezione = 0 come prodotto scalare tra vettore normale
+    // al piano e punto generico della frattura (scelto il baricentro)
     d1 = n1[0] * b1[0] + n1[1] * b1[1]  + n1[2] * b1[2];
     d2 = n2[0] * b2[0] + n2[1] * b2[1]  + n2[2] * b2[2];
 
     Vector3d vec = n1.cross(n2);
-    //parametrizzo la z = t
-    // t individua la direzione della retta di intersezione tra i due piani. dobbiamo trovare un Point qualsiasi che sta su quella retta,
-    //    quindi un suo parametro può essere scelto a piacere. Scelgo la z del Point come parametro libero, ma non lo posso fare se la t[2] = 0, qiundi selezione diversa se t[2] = 0
-    //if (t[2]>1e-14)//se t[2] != 0
+    // parametrizzo la z = t
+    // t individua la direzione della retta di intersezione tra i due piani. Si trova un Point qualsiasi sulla retta,
+    // quindi un suo parametro può essere scelto a piacere. Si sceglie la z del Point come parametro libero, ma si può fare se la t[2]≠0; dunque selezione diversa se t[2]=0
+    // if (t[2]>1e-14)//se t[2] != 0
     //{
-    // Faccio quel che ho fTTO ALL'INIZIO
-    // Create the matrix A and initialize it with vectors n1, n2, and t
+    // Crea la matrice A e la inizializza con i vettori  n1, n2, t
     Matrix3d A;
     A << n1[0], n1[1],n1[2],
         n2[0], n2[1],n2[2],
         t[0], t[1], t[2];
     //A.row(2) = t;
 
-    //risolviamo il sistema
-    // det(A) != 0 : per verificare che il istema abbia soluzione = i due piani si intersecano
-    // aggiungere non complanarity! controllo che il modulo di t sia diverso da 0 per evitare prodotto vettoriale != 0
+    // risoluzione sistema
+    // det(A) != 0 : per verificare che il sistema abbia soluzione (i due piani si intersecano)
+    // controllo che il modulo di t sia diverso da 0 per evitare prodotto vettoriale != 0
 
     // Verifica della possibilità di intersezione
     if (vec.dot(t) > tolDefault) {
@@ -131,27 +130,23 @@ bool intersezione_piani (Vector3d& n1,
         Vector3d B;
         B << d1, d2,0;
         Point = A.colPivHouseholderQr().solve(B); //(x,y,t)
-
         return true;
 
     } else {
         // I piani sono paralleli o coincidenti
-
         return false;
     }
 
     /*}else{//se t[2] == 0
-        //devo scegliere un altro parametro libero rispetto a quello che
-        // Create the matrix A and initialize it with vectors n1, n2, and t
+        // scelta di un altro parametro libero
         Matrix2d A;
         A << n1[0], n1[2],
             n2[0], n2[2];
 
         //A.row(2) = t;
 
-        //risolviamo il sistema
-        // det(A) != 0 : per verificare che il istema abbia soluzione = i due piani si intersecano
-        // aggiungere non complanarity! controllo che il modulo di t sia diverso da 0 per evitare prodotto vettoriale != 0
+        // risoluzione sistema
+        // det(A) != 0 :
 
         // Verifica della possibilità di intersezione
         if (vec.dot(t) > 1e-14) {
@@ -174,10 +169,10 @@ bool intersezione_piani (Vector3d& n1,
 
 //troviamo il punto di intersezione tra la retta (Point, t)  e la retta di prolungamento del segmento V1V2
 bool intersezione_rette (Vector3d& t,
-                          Vector3d& V1,
-                          Vector3d& V2,
-                          Vector3d& Point,
-                          Vector3d& Punto0)
+                        Vector3d& V1,
+                        Vector3d& V2,
+                        Vector3d& Point,
+                        Vector3d& Punto0)
 {
 
     Vector3d vettoreDirezioneI = V1 - V2;
@@ -198,8 +193,6 @@ bool intersezione_rette (Vector3d& t,
 
         int rankC = Completa_.fullPivLu().rank();
 
-
-        //cout << "\n\nIl rango della matrice A è: " << rank << std::endl;
         if (rankA == rankC && rankA == 2) {
             VectorXd sol_;
             if (A_.rows() >= A_.cols()) {
@@ -226,53 +219,45 @@ bool intersezione_rette (Vector3d& t,
         //}
     //}
     //else{ //terza componente di t != 0*/
-    MatrixXd A(3,2);
-    A.col(0) << t;
-    A.col(1) << vettoreDirezioneI;
+    // si risolve un problema 2x2 escludendo la componente minima di t
+    int pos = 0;
+    if(abs(t[1]) < abs(t[0]) + tolDefault){
+        pos = 1;
+    }
+    if(abs(t[2]) < (t[pos]) + tolDefault){
+        pos = 2;
+    }
 
-    MatrixXd Completa(3,3); //è A con ultima colonna termine noto
-    Completa.col(0) << t;
-    Completa.col(1) << vettoreDirezioneI;
-    Completa.col(2) << termine_noto;
-
-    // Calcola il rango utilizzando la decomposizione LU
-    //int rankA = A.fullPivLu().rank();
-
-    //int rankC = Completa.fullPivLu().rank();
+    Matrix2d A;
+    Vector2d b;
+    if (pos == 0) {
+        A << t[1], vettoreDirezioneI[1],
+            t[2], vettoreDirezioneI[2];
+        b << termine_noto[1], termine_noto[2];
+    } else if (pos == 1) {
+        A << t[0], vettoreDirezioneI[0],
+            t[2], vettoreDirezioneI[2];
+        b << termine_noto[0], termine_noto[2];
+    } else {
+        A << t[0], vettoreDirezioneI[0],
+            t[1], vettoreDirezioneI[1];
+        b << termine_noto[0], termine_noto[1];
+    }
 
     Vector3d vettopa = vec_product(t,vettoreDirezioneI);
     double norm = vettopa.norm();
 
-    //cout << "\n\nIl rango della matrice A è: " << rank << std::endl;
     if (norm>tolDefault) {
-        VectorXd sol;
-        if (A.rows() >= A.cols()) {
+        Vector2d sol;
+        sol = A.colPivHouseholderQr().solve(b);
 
-            /*std::chrono::steady_clock::time_point t_begin= chrono::steady_clock::now();
-            sol = A.jacobiSvd(Eigen::ComputeThinU | Eigen::ComputeThinV).solve(termine_noto);
-            std::chrono::steady_clock::time_point t_end= chrono::steady_clock::now();
-            double duration = chrono::duration_cast<chrono::microseconds>(t_end - t_begin).count();
-            cout<<"\tjacobisvd.solve time: "<<duration<<" microseconds\n" <<endl;
-            */
+        // Calcola il punto di intersezione Punto0
+        Punto0 = Point + sol[0] * t ;
+        return true;
 
-            //chrono::steady_clock::time_point t_begin2= chrono::steady_clock::now();
-            sol = (A.transpose() * A).ldlt().solve(A.transpose() * termine_noto);
-            //chrono::steady_clock::time_point t_end2= chrono::steady_clock::now();
-            //double duration2 = chrono::duration_cast<chrono::microseconds>(t_end2 - t_begin2).count();
-            //cout<<"\tother time: "<<duration2<<" microseconds\n" <<endl;
-
-            //ora abbiamo trovato i coeffieiente s e aplha
-
-            // Calcola il punto di intersezione Punto0
-            Punto0 = Point + sol[0] * t ;
-            return true;
-        }
     }
 
-
     return false;
-
-
 
 }
 
